805a520b6a9f288715c88bd20ae79c3a3b03a48a
Darren Griffitt <darrengriffitt@gmail.com>
2024-04-08 01:49:52 -0500

    Cart is mostly functioning now - - Items are no longer loosely attached. - Cart JS file completely reworked with a new Item class. - New Item class is instantiable. - Script does not have documentation yet, though functionality should remain similar for calling functions. - Added svgs to a new directory "cart-assets" under images. - Product images shall now be stored in "images/product-images" - Shopping cart will not function without the database, and has no current way of handling no items in the list; to be added soon. - Refactored the cart.php file; much smaller now. - Ajax added to cart near the end of the cart.js script for retrieving item information.

---
+++ src/php/get-cart-data.php
@@ -0,0 +1,27 @@
+<?php
+// this regex will remove all underscores from property names replace(/"_([^"]+)":/g, '"$1":'));
+
+// SQL to grab cart data.
+$justCartItem = $_GET["justCart"] ?? true;
+include ("connect-db.php");
+
+if ($justCartItem) {
+    $sql = "SELECT
+            item.item_id, item.name, item.price, item.description, item.category, item.image, 
+            meta.alt_text
+            FROM item
+            INNER JOIN meta
+            ON item.meta_id = meta.meta_id";
+            
+    $statement = $db->prepare($sql);
+
+    if ($statement->execute()) {
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+        $success = true;
+    } else {
+        $success = false;
+    }
+
+    echo json_encode($results);
+}
