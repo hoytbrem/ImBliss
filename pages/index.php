51df0c29f9ab01f9b17119bded5cc9ddf98be115
Hoyt Brem <hoytbrem@gmail.com>
2024-04-15 17:53:03 -0500

    finished hero and featured products

--- pages/index.php
+++ pages/index.php
@@ -17,6 +17,7 @@
     <meta name="description"
         content="We sell environmentally friendly, home-grown snacks & treats that serve as a delicious reminder that healthy doesn't have to taste bad at all." />
     <meta name="keywords" content="healthy, snacks, nutritious" />
+    <link rel="stylesheet" type="text/css" href="../theme/main-page.css" />
 
     <?php include("partial/every-page.html"); ?>
 </head>
@@ -25,136 +26,97 @@
 
     <?php include("./partial/nav.php"); ?>
     <?php // <!-- Other Includes -->
-    include ("./partial/cart.php"); // Cart ?>
+    /*include ("./partial/cart.php");*/ // Cart ?>
 
 
     <!-- Hero Section -->
-    <!-- VERY WORK IN PROGRESS -->
-    <section id="hero">
-        <div id="hero-wrapper">
-            <div id="hero-title">
-                <h2>Featured Product Here</h2>
-            </div>
-            <div id="hero-main">
-                <!-- Hero Left -->
-                <div id="hero-left">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Leo in vitae turpis massa sed elementum. Integer enim neque
-                        volutpat ac tincidunt vitae semper quis. Sit amet nulla facilisi morbi tempus iaculis. Dolor
-                        morbi non arcu risus quis varius quam quisque. Et ultrices neque ornare aenean euismod elementum
-                        nisi quis. Sapien nec sagittis aliquam malesuada bibendum. Nam aliquam sem et tortor consequat
-                        id. Laoreet sit amet cursus sit amet. Eget gravida cum sociis natoque penatibus et. Pharetra sit
-                        amet aliquam id diam maecenas ultricies. Nisl rhoncus mattis rhoncus urna.</p>
-                    <button>View</button>
-                    <button>Add to Cart</button>
+    <div class="jumbotron d-flex align-items-center" id="hero-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1 class="display-4">Featured product here</h1>
+                    <p class="lead">Lorem ipsum sample text</p>
+                    <p>
+                        <a class="btn btn-primary btn-lg" id="hero-section-view-button" href="#" role="button">View</a>
+                        <a class="btn btn-secondary btn-lg" id="hero-section-add-to-cart-button" href="#"
+                            role="button">Add to Cart</a>
+                    </p>
                 </div>
-                <!-- Hero Right -->
-                <div id="hero-right">
-                    <img src="../images/main/test-image.jpeg" id="hero-secondary-img">
+                <div class="col-md-6">
+                    <img src="../images/main/imBliss-Pecan-Cherry-Energy-Bites.jpg" class="img-fluid"
+                        id="hero-section-image" alt="Product Image">
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 
-    <!-- Main Content -->
-    <main>
-        <!-- Featured Products -->
-        <section id="featured-products">
-            <img src="../images/main/test-image.jpeg">
-            <img src="../images/main/test-image.jpeg">
-            <img src="../images/main/test-image.jpeg">
-        </section>
+    <!-- Product Images Section -->
+    <div class="container my-4">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="../images/main/ImBliss-Premium-Package2.jpg" class="img-fluid product-mockups"
+                    alt="Product Image">
+            </div>
+            <div class="col-md-4">
+                <img src="../images/main/ImBliss-Tote-Bag.jpg" class="img-fluid product-mockups" alt="Product Image">
+            </div>
+            <div class="col-md-4">
+                <img src="../images/main/ImBliss-Water-Bottle.jpg" class="img-fluid product-mockups"
+                    alt="Product Image">
+            </div>
+        </div>
+    </div>
 
-        <!-- Nutritional Benefits -->
-        <section id="nutritional-benefits">
-            <div id="nutritional-benefits-wrapper">
-                <div id="nutritional-benefits-left">
-                    <h2>Nutritional Benefits</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Leo in vitae turpis massa sed elementum. Integer enim neque
-                        volutpat ac tincidunt vitae semper quis. Sit amet nulla facilisi morbi tempus iaculis. Dolor
-                        morbi non arcu risus quis varius quam quisque. Et ultrices neque ornare aenean euismod elementum
-                        nisi quis. Sapien nec sagittis aliquam malesuada bibendum. Nam aliquam sem et tortor consequat
-                        id. Laoreet sit amet cursus sit amet. Eget gravida cum sociis natoque penatibus et. Pharetra sit
-                        amet aliquam id diam maecenas ultricies. Nisl rhoncus mattis rhoncus urna.</p>
-                    <img src="../images/main/test-image.jpeg">
+    <!-- Nutritional Benefits Section -->
+    <div class="bg-light py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Nutritional benefits</h2>
+                    <p>Lorem ipsum sample text</p>
+                    <img src="../images/main/healthy-natural-vegetables-display.jpg" class="img-fluid" alt="Image">
                 </div>
-                <div id="nutritional-benefits-right">
-                    <img src="../images/main/test-image.jpeg">
+                <div class="col-md-6">
+                    <img src="your-image-url" class="img-fluid" alt="Image">
                 </div>
             </div>
-        </section>
+        </div>
+    </div>
 
-        <!-- Main Page Learn More -->
-        <section id="main-page-learn-more">
-            <!-- Left Side -->
-            <div class="main-page-learn-more-box">
-                <h2 class="orange">
-                    New Arrivals
-                </h2>
-                <button class="green">Explore</button>
+    <!-- New Arrivals and Planet Section -->
+    <div class="container my-4">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>New Arrivals</h2>
+                <a class="btn btn-primary" href="#" role="button">Explore</a>
             </div>
-            <!-- Right Side -->
-            <div class="main-page-learn-more-box">
-                <h2 class="green">
-                    Dedicated to Our Planet
-                </h2>
-                <button class="orange">Learn More</button>
+            <div class="col-md-6">
+                <h2>Dedicated to our planet</h2>
+                <a class="btn btn-primary" href="#" role="button">Learn More</a>
             </div>
-        </section>
+        </div>
+    </div>
 
-        <!-- Client Testimonials -->
-        <section id="client-testimonials">
-            <h2>Client Testimonials</h2>
-            <div id="client-testimonials-wrapper">
-                <div class="client-testimonial">
-                    <h3>"Best Product Ever!"</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Leo in vitae turpis massa sed elementum. Integer enim neque
-                        volutpat ac tincidunt vitae semper quis. Sit amet nulla facilisi morbi tempus iaculis. Dolor
-                        morbi non arcu risus quis varius quam quisque. Et ultrices neque ornare aenean euismod elementum
-                        nisi quis. Sapien nec</p>
-                    <p>Name Here</p>
-                </div>
-                <div class="client-testimonial">
-                    <h3>"Best Product Ever!"</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Leo in vitae turpis massa sed elementum. Integer enim neque
-                        volutpat ac tincidunt vitae semper quis. Sit amet nulla facilisi morbi tempus iaculis. Dolor
-                        morbi non arcu risus quis varius quam quisque. Et ultrices neque ornare aenean euismod elementum
-                        nisi quis. Sapien nec</p>
-                    <p>Name Here</p>
-                </div>
-                <div class="client-testimonial">
-                    <h3>"Best Product Ever!"</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Leo in vitae turpis massa sed elementum. Integer enim neque
-                        volutpat ac tincidunt vitae semper quis. Sit amet nulla facilisi morbi tempus iaculis. Dolor
-                        morbi non arcu risus quis varius quam quisque. Et ultrices neque ornare aenean euismod elementum
-                        nisi quis. Sapien nec</p>
-                    <p>Name Here</p>
-                </div>
-                <div class="client-testimonial">
-                    <h3>"Best Product Ever!"</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Leo in vitae turpis massa sed elementum. Integer enim neque
-                        volutpat ac tincidunt vitae semper quis. Sit amet nulla facilisi morbi tempus iaculis. Dolor
-                        morbi non arcu risus quis varius quam quisque. Et ultrices neque ornare aenean euismod elementum
-                        nisi quis. Sapien nec</p>
-                    <p>Name Here</p>
-                </div>
+    <!-- Client Testimonials Section -->
+    <div class="container my-4">
+        <h2 class="text-center">Client Testimonials</h2>
+        <div class="row">
+            <!-- Repeat this div for each testimonial -->
+            <div class="col-md-3">
+                <h5>Title</h5>
+                <p>Lorem ipsum sample text</p>
+                <p><small>- Name</small></p>
             </div>
-        </section>
+        </div>
+    </div>
 
-        <!-- Sigh Up Promo -->
-        <section id="sign-up-promo">
-            <div>
-                <h3>Sigh Up Today & Get</h3>
-                <h1>15% OFF</h1>
-                <h2>Your First Purchase</h2>
-            </div>
-            <button>Sign Up</button>
-        </section>
+    <!-- Sign Up Section -->
+    <div class="jumbotron text-center">
+        <h1 class="display-4">Sign up today and get</h1>
+        <p class="lead">15% off</p>
+        <p>Your first purchase</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a></p>
+    </div>
     </main>
     <?php include("./partial/footer.html"); ?>
 </body>
